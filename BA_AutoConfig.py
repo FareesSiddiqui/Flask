d28776950582ec61d390ff50e95611de5c5a9dc9
Farees <44035032+FareesSiddiqui@users.noreply.github.com>
2023-11-13 13:18:27 -0500

    Update BA_AutoConfig.py

--- BA_AutoConfig.py
+++ BA_AutoConfig.py
@@ -2,30 +2,41 @@ import flask
 import jinja2
 import re
 import time
+from pprint import pprint
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path="/static")
+
+indexes = {}
 
 # TODO: Make a page for the static config, this way we can change both aspects of the config without clutering the page
 
+# TODO: Fix bug where clicking the static page button twice sends us to a weird path and gives a 404 error the path is static/static/static_config.html when it should be static/static_config.html
 
 def get_vars(template):
     with open(template, "r") as template_file:
         lines = list(template_file.readlines())
     variables = []
+    
     for i in range(len(lines)):
+        indexes[i] = lines[i]
         matched = re.search(r"############## ------------------->", lines[i])
+        
+        static_vars = list(indexes.values())
         
         if matched:
             variables.append(lines[i][:matched.start()-1])
+            
+        if lines[i] in static_vars:
+            static_vars.remove(lines[i])
     
-    return variables
+    return variables, static_vars
 
 def validate_ip(ip):
     # TODO: check if ip is in correct range
     # TODO: check if ip location is correct (ex, not using a Paris IP for a device in Toronto)
     pass
 
-def generate_html_form(variable_names):
+def generate_html_form(variable_names, file_path):
     html = f"""<html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -43,6 +54,8 @@ def generate_html_form(variable_names):
             <p class="subtitle">
                 A tool for automatically generating config scripts for devices on the BA network
             </p>
+            <a class="button is-link is-responsive is-outlined is-rounded" href="/">Dynamic Config</a>  <a class="button is-link is-responsive is-outlined is-rounded" href="static/static_config.html">Static Config</a>
+
         </center>
     </div>
     </section>
@@ -79,12 +92,13 @@ def generate_html_form(variable_names):
 </html>
     """
 
-    with open("templates/index.html", "w") as html_file:
+    with open(file_path, "w") as html_file:
         html_file.write(html)
 
-variable_names = get_vars("templates/config_template.txt")
-generate_html_form(variable_names)
-print(variable_names)
+dynamic_config_variable_names, static_config_vairable_names = get_vars("templates/config_template.txt")
+generate_html_form(dynamic_config_variable_names, "templates/index.html")
+generate_html_form(static_config_vairable_names, "static/static_config.html")
+print(dynamic_config_variable_names)
 
 time.sleep(0.5)
 
@@ -92,4 +106,6 @@ time.sleep(0.5)
 def hello_world():
     return flask.render_template("index.html")
 
-app.run(debug=True)
+pprint(indexes)
+
+app.run()
