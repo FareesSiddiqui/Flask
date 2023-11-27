aea9d275db704ff6443ee3511fcd936355e90b10
Farees <44035032+FareesSiddiqui@users.noreply.github.com>
2023-11-27 14:29:12 -0500

    Update BA_AutoConfig.py

--- BA_AutoConfig.py
+++ BA_AutoConfig.py
@@ -16,18 +16,19 @@ def get_vars(template):
     with open(template, "r") as template_file:
         lines = list(template_file.readlines())
     variables = []
+    static_vars = []
+    static_lines = lines.copy()
     
     for i in range(len(lines)):
         indexes[i] = lines[i]
         matched = re.search(r"############## ------------------->", lines[i])
         
-        static_vars = list(indexes.values())
-        
         if matched:
+            static_lines.remove(lines[i])
             variables.append(lines[i][:matched.start()-1])
-            
-        if lines[i] in static_vars:
-            static_vars.remove(lines[i])
+
+        else:
+            static_vars.append(lines[i])
     
     return variables, static_vars
 
@@ -36,7 +37,7 @@ def validate_ip(ip):
     # TODO: check if ip location is correct (ex, not using a Paris IP for a device in Toronto)
     pass
 
-def generate_html_form(variable_names, file_path):
+def generate_html_form(variable_names, file_path, prev_entries):
     html = f"""<html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -64,18 +65,18 @@ def generate_html_form(variable_names, file_path):
     <form>
     """
     
-    for variable_name in variable_names:
-        label = variable_name
-        input_id = variable_name
+    for i in range(len(variable_names)):
+        label = variable_names[i]
+        input_id = variable_names[i]
         html += f"""
         <div class="columns">
             <div class="column is-one-half">
-                <label for="{input_id}">{label}: </label>
+                <label for="{input_id}">{label} </label>
             </div>
             <div class="column">
                 <div class="field">
                     <p class="control">
-                        <input class="input is-primary" type="text" id="{input_id}" name="{input_id}">
+                        <input class="input is-primary" type="text" id="{input_id}" name="{input_id}" value='{prev_entries[i]}'>
                     </p>
                 </div>
             </div>
@@ -88,6 +89,63 @@ def generate_html_form(variable_names, file_path):
             </div>
         </form>
     </div>
+    <script src="static/js/app.js"></script>
+</body>
+</html>
+    """
+
+    with open(file_path, "w") as html_file:
+        html_file.write(html)
+        
+def generate_static_form(variable_names, file_path, prev_entries):
+    html = f"""<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+</head>
+<body>
+
+    <section class="hero is-warning">
+    <div class="hero-body">
+        <center>
+            <p class="title">
+                BA Auto Config
+            </p>
+            <p class="subtitle">
+                A tool for automatically generating config scripts for devices on the BA network
+            </p>
+            <a class="button is-link is-responsive is-outlined is-rounded" href="/">Dynamic Config</a>  <a class="button is-link is-responsive is-outlined is-rounded" href="static/static_config.html">Static Config</a>
+
+        </center>
+    </div>
+    </section>
+
+    <div class="container" style="width: 50%; padding-top: 3vh">
+    <form>
+    """
+    
+    for i in range(len(variable_names)):
+        input_id = variable_names[i]
+        html += f"""
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <p class="control">
+                        <input class="input is-primary" type="text" id="{input_id}" name="{input_id}" value='{prev_entries[i]}'>
+                    </p>
+                </div>
+            </div>
+        </div>
+        """
+
+    html += """
+            <div class="control" style="padding-top: 1vh">
+                <button class="button is-link is-light" id="submit_button">Submit</button>
+            </div>
+        </form>
+    </div>
+    <script src="static/js/app.js"></script>
 </body>
 </html>
     """
@@ -95,10 +153,9 @@ def generate_html_form(variable_names, file_path):
     with open(file_path, "w") as html_file:
         html_file.write(html)
 
-dynamic_config_variable_names, static_config_vairable_names = get_vars("templates/config_template.txt")
-generate_html_form(dynamic_config_variable_names, "templates/index.html")
-generate_html_form(static_config_vairable_names, "static/static_config.html")
-print(dynamic_config_variable_names)
+dynamic_config_variable_names, static_config_vairable_names= get_vars("templates/config_template.txt")
+generate_html_form(dynamic_config_variable_names, "templates/index.html", ["" for i in range(len(static_config_vairable_names))])
+generate_static_form(static_config_vairable_names, "static/static_config.html", static_config_vairable_names)
 
 time.sleep(0.5)
 
